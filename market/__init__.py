de2eb809d4b7b74d25bef072443eb26ac99f6201
Fer-Bar <linofernando2703@gmail.com>
2022-04-28 23:32:38 -0400

    Create branch for deploy

--- market/__init__.py
+++ market/__init__.py
@@ -3,6 +3,9 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DB_URI')
--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from market import app
+from dotenv import load_dotenv
 
+load_dotenv()
 #Checks if the run.py file has executed directly and not imported
 if __name__ == '__main__':
 
